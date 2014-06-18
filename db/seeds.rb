8d58f734438a599256d57207acdd47d55a8648bb
justindruiz <justinruizwrestling@gmail.com>
2014-06-18 17:00:00 -0600

    Added comments to blog

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 Post.create(title: 'My first post', author: 'Justin Ruiz', description: 'This is the first post on this blog. Hello World!')
 Post.create(title: 'My second post', author: 'Justin Ruiz', description: 'This is the second post on this blog. Hello World!')
 Post.create(title: 'My third post', author: 'Justin Ruiz', description: 'This is the third post on this blog. Hello World!')
+
+Comment.create(author: 'George W. Bush', message: 'Whoops! Iraq.', post_id: 2)
+Comment.create(author: 'Dick Cheney', message: 'Merica!', post_id: 2)
+Comment.create(author: 'Obama', message: '.......', post_id: 2)
