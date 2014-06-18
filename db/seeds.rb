8a606b08e056cbf91a2119a67ba1ab27374b6084
justindruiz <justinruizwrestling@gmail.com>
2014-06-18 14:14:00 -0600

    Switched postgresql in for sqlite3 and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: 'My first post', author: 'Justin Ruiz', description: 'This is the first post on this blog. Hello World!')
+Post.create(title: 'My second post', author: 'Justin Ruiz', description: 'This is the second post on this blog. Hello World!')
+Post.create(title: 'My third post', author: 'Justin Ruiz', description: 'This is the third post on this blog. Hello World!')
